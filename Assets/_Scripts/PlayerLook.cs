cec5a4f743aff09640c353558d096dbb8e0fa2e0
Tefgoo <tefgoov@gmail.com>
2023-03-03 20:55:51 -0600

    Alpha Shooting
    Cube as gun and bullets spawn on click

--- Assets/_Scripts/PlayerLook.cs
+++ Assets/_Scripts/PlayerLook.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class PlayerLook : MonoBehaviour
 {
     public Camera cam;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawnPoint;
 
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+
         Vector3 mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.transform.position.y));
         Vector3 lookDir = mousePos - transform.position;
         lookDir.y = 0f;
         Quaternion targetRotation = Quaternion.LookRotation(lookDir);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 360f);
+    }
+
+    void Shoot()
+    {
+        // Instantiate a new bullet at the bullet spawn point
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+
+        // Get the direction the player is facing (the direction of the mouse)
+        Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - bullet.transform.position;
+        direction.z = 0;
+
+        // Set the velocity of the bullet to be in the direction the player is facing
+        bullet.GetComponent<Rigidbody>().velocity = direction.normalized * 10f;
     }
 }
