f80ab3a57b6e6ac43364ef70bea07602f13de97e
Tefgoo <tefgoov@gmail.com>
2023-03-23 12:35:47 -0500

    Improved Spawner
    New Spawn system like Bo2 zombies, and adjusted enemies attack range

--- Assets/_Scripts/EnemySpawner.cs
+++ Assets/_Scripts/EnemySpawner.cs
@@ -1,61 +1,61 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class EnemySpawner : MonoBehaviour
 {
 
-    public GameObject enemyPrefab;
-    public int numEnemiesPerRound = 10;
-    public float spawnInterval = 1f;
-    public Transform[] spawnPoints;
-    public int numRounds = 5;
+    public GameObject enemyPrefab; // Enemy prefab to spawn
+    public float spawnInterval = 3f; // Time between spawns
+    public int enemiesPerWave = 10; // Number of enemies to spawn in each wave
+    public float timeBetweenWaves = 10f; // Time between waves
+    public Transform[] spawnPoints; // Array of spawn points for enemies
+    public int currentWave = 0; // Current wave number
+    public int enemiesRemaining; // Number of enemies remaining in the current wave
+    public float timeUntilNextWave; // Time until the next wave begins
+    public bool spawningEnemies = false; // Whether enemies are currently being spawned
 
-    private int currentRound = 0;
-    private int numEnemiesRemaining = 0;
-
-    private void Start()
+    void Start()
     {
-        StartNewRound();
+        timeUntilNextWave = timeBetweenWaves;
     }
 
-    private IEnumerator SpawnEnemies()
+    void Update()
     {
-        for (int i = 0; i < numEnemiesPerRound; i++)
+        if (!spawningEnemies)
         {
-            int spawnIndex = Random.Range(0, spawnPoints.Length);
-            Instantiate(enemyPrefab, spawnPoints[spawnIndex].position, spawnPoints[spawnIndex].rotation);
-            numEnemiesRemaining++;
-            yield return new WaitForSeconds(spawnInterval);
+            if (enemiesRemaining == 0)
+            {
+                // Start a new wave
+                currentWave++;
+                enemiesRemaining = enemiesPerWave * currentWave;
+                timeUntilNextWave = timeBetweenWaves;
+            }
+            else
+            {
+                // Wait for the current wave to end
+                timeUntilNextWave -= Time.deltaTime;
+                if (timeUntilNextWave <= 0f)
+                {
+                    SpawnEnemy();
+                    timeUntilNextWave = spawnInterval;
+                }
+            }
         }
     }
 
-    private void Update()
+    void SpawnEnemy()
     {
-        if (numEnemiesRemaining == 0)
+        // Pick a random spawn point
+        int spawnIndex = Random.Range(0, spawnPoints.Length);
+        // Spawn the enemy at the chosen spawn point
+        Instantiate(enemyPrefab, spawnPoints[spawnIndex].position, spawnPoints[spawnIndex].rotation);
+        // Decrement the number of enemies remaining
+        enemiesRemaining--;
+        if (enemiesRemaining == 0)
         {
-            StartCoroutine(StartNextRound());
+            // Stop spawning enemies when the wave is finished
+            spawningEnemies = false;
         }
-    }
-
-    private IEnumerator StartNextRound()
-    {
-        currentRound++;
-        if (currentRound > numRounds)
-        {
-            SceneManager.LoadScene("Victory");
-        }
-        else
-        {
-            StartNewRound();
-        }
-        yield return null;
-    }
-
-    private void StartNewRound()
-    {
-        numEnemiesRemaining = 0;
-        StartCoroutine(SpawnEnemies());
     }
 }
